1bc269e3f266b4e7c6b37cb3fa80393b11d37498
asher <243485908@qq.com>
2019-08-25 16:59:48 +0800

    更改成jquery

--- js/api.js
+++ js/api.js
@@ -1,7 +1,19 @@
 /**
  * 请求挂载到Vue原型链中
  */
-Vue.prototype.$http = axios.create({
-    baseURL: '/api/',
-    timeout: 50000
-});
+window.$http = (type, url, data, success, error) => {
+    url = 'http://172.16.0.207:8882/api/' + url;
+    return $.ajax({
+        url: url,
+        type: type,
+        dateType: 'json',
+        headers: {'Content-Type': 'application/json;charset=utf8'},
+        data: data ? JSON.stringify(data) : null,
+        success: function (data) {
+            success(data)
+        },
+        error: function (data) {
+            error(data);
+        }
+    })
+}
